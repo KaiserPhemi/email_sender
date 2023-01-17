80a6f06b3168784b7a2f56a8d2f83381255b7f11
KaiserPhemi <kaiser.phemi@protonmail.ch>
2023-01-17 18:26:24 +0100

    chore()

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ This is just a test message
 """
 email_msg = EmailMessage()
 email_msg['From'] = email_sender
-email_msg['To'] = email_receiver
+email_msg['To'] = email_receiver 
 email_msg['subject'] = subject
 email_msg.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context) as smtp:
+  smtp.login(email_sender, email_password)
+  smtp.sendmail(email_sender, email_receiver, email_msg.as_string())
